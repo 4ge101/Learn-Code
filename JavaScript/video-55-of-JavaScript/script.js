22d1e9c8de1bd0951fcc951f947572a604b7db7f
4ge101 <theforgettenone2@gmail.com>
2024-07-28 19:33:31 +0545

    learning varables in JS

---
+++ JavaScript/video-55-of-JavaScript/script.js
@@ -0,0 +1,15 @@
+console.log("hey this message is this going in the console?")
+
+var a = 5; // var is used to make variables
+var b = 10;
+var c = "Hello World";
+var _d = "sami";
+var $e = "ali";
+
+console.log($e)
+console.log(_d)
+console.log(a + b + 5)
+console.log(typeof a, typeof b, typeof c, typeof _d, typeof $e) //it is used to know data type of variables
+
+// const a1 = 5;
+// a1 = a1 + 1;  //after apply const we cant cant change it's not allowed
