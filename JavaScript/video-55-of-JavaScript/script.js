d412e78d6a1144a1be2ebdef48161a00f1557911
4ge101 <theforgettenone2@gmail.com>
2024-07-28 21:24:52 +0545

    learning varables in JS

--- JavaScript/video-55-of-JavaScript/script.js
+++ JavaScript/video-55-of-JavaScript/script.js
@@ -1,15 +1,21 @@
 console.log("hey this message is this going in the console?")
 
 var a = 5; // var is used to make variables
-var b = 10;
-var c = "Hello World";
-var _d = "sami";
-var $e = "ali";
+// a = a + 1
+let b = 10;
+let c = "Hello World";
+let _d = "sami";
+let $e = "ali";
 
-console.log($e)
-console.log(_d)
-console.log(a + b + 5)
-console.log(typeof a, typeof b, typeof c, typeof _d, typeof $e) //it is used to know data type of variables
+// console.log($e)
+// console.log(_d)
+// console.log(a + b + 5)
+// console.log(typeof a, typeof b, typeof c, typeof _d, typeof $e) //it is used to know data type of variables
 
+{
+    // let a = 66; //first output is 66 and then 5 because let is blocked scoped if there is var except let then it's output comes 66 because it is global
+    console.log(a)
+}
+console.log(a)
 // const a1 = 5;
 // a1 = a1 + 1;  //after apply const we cant cant change it's not allowed
