130e0b5e710cae6cb8ff10caca182d51968a5f56
4ge101 <theforgettenone2@gmail.com>
2024-07-28 22:25:42 +0545

    practise

---
+++ HTML WITH CSS PRACTISE/making simple portfolio using html and css/script.js
@@ -0,0 +1,16 @@
+// var a = prompt("Enter your Name?")
+// console.log("Your Name is " + a)
+
+// console.log("Did wbsite working well")
+
+// var isTrue = confirm("Did you want to see this page because it is just a demo website")
+
+// if (isTrue) {
+//     alert("Now you can see this demo portfolio website")
+//     console.log("Now you can see this demo portfolio website")
+// }
+
+// else {
+//     alert("You dont want to see this site?")
+//     console.log("You dont want to see this site?")
+// }
