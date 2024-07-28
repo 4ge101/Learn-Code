78815e7672f122a9985f0d3994e2632716d172bb
4ge101 <theforgettenone2@gmail.com>
2024-07-28 09:43:33 +0545

    learning javascript consloe & prompt

---
+++ JavaScript/Starting-JavaScript-video-54/new.js
@@ -0,0 +1,3 @@
+console.log("Hello World");
+console.log("how are you");
+console.log("Code is running now fast..."); //it is used for go outut in cnosle
---
+++ JavaScript/Starting-JavaScript-video-54/script.js
@@ -0,0 +1,19 @@
+alert("Who are you?");  // to give alert messages
+
+console.log("Code is slowly runing...");
+console.log("Code is runnng...");
+console.log("Code is running now fast..."); //it is used for go outut in cnosle
+
+var a = prompt("enter your Number") // It displays a dialoug box and user can give his input
+
+console.log("your number is " + a)
+
+var isTrue = confirm("Are you sure you want to leave this page")
+
+if (isTrue) {
+    console.log("now you are leaving this page")
+}
+
+else {
+    console.log("now you are satying in this page")
+}
