89d7229b8e93d71d2e3d17c18f63a79ee6d58362
4ge101 <theforgettenone2@gmail.com>
2024-07-29 08:40:30 +0545

    learning about datatypes and variables in js

--- JavaScript/video-55-of-JavaScript/script.js
+++ JavaScript/video-55-of-JavaScript/script.js
@@ -1,4 +1,4 @@
-console.log("hey this message is this going in the console?")
+console.log("hey is this message going in the console?")
 
 var a = 5; // var is used to make variables
 // a = a + 1
@@ -19,3 +19,26 @@ let $e = "ali";
 console.log(a)
 // const a1 = 5;
 // a1 = a1 + 1;  //after apply const we cant cant change it's not allowed
+
+
+let x = "Ali Sami"; // string
+let y = 50; // number
+let z = 4.44; // number
+const p = true; // boolean
+let q = undefined; // undefined
+let r = null; // object
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name": "Sami",
+    "job code": 4000,
+    "is_handsome": true
+}
+
+console.log(o);
+o.salary = "10lakhs"; //this is used to add more object about this person
+console.log(o);
+o.salary = "crores";
+console.log(o);
