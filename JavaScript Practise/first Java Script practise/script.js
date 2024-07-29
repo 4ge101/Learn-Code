9b4e0d0807556aefac6ee08e04eb2266358d9ef6
4ge101 <theforgettenone2@gmail.com>
2024-07-29 11:09:01 +0545

    practing javascript

---
+++ JavaScript Practise/first Java Script practise/script.js
@@ -0,0 +1,32 @@
+let a = "number"
+let b = 2
+
+console.log(a + b)
+
+let o = {
+"name": "sami",
+"salary": 200
+}
+
+console.log(o)
+
+//data types
+console.log(a, b)
+console.log(typeof a, typeof b)
+
+const a9 = 5
+// a9 = 1 + 5
+
+const a8 = 10
+
+// dictanory
+
+let dict = {
+    "Name": "ali Sami",
+    "Age": 12,
+
+    "Grade": 8,
+    "Skills": "JavaScript"
+}
+
+console.log(dict)
