8a6b2b359f2a934ee796f82a7bdb2b2bc715c210
4ge101 <theforgettenone2@gmail.com>
2024-07-31 19:56:39 +0545

    learning if & else

---
+++ JavaScript/video 56 of JavaScript/index.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+let age = 3;
+let grace = 2;
+
+console.log(age + grace)
+console.log(age * grace)
+console.log(age - grace)
+console.log(age / grace)
+console.log(age ** grace)
+
+if((age+grace)>20){
+    console.log("You can visit our website");
+}
+
+else{
+    console.log("Sorry you cant visit our website");
+}
+
+// IN top statement we crated a variable age 40 then we say that "if" age is grater then 20 then show "you can visit our website" else show "you cant visit our website"
