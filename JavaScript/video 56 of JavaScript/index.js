5e113f3c9fe096c6f09dfae53e97db7daddeda75
4ge101 <theforgettenone2@gmail.com>
2024-08-01 15:05:40 +0545

    learning if else & operators

--- JavaScript/video 56 of JavaScript/index.js
+++ JavaScript/video 56 of JavaScript/index.js
@@ -1,20 +1,57 @@
 console.log("Hello World");
 
-let age = 3;
-let grace = 2;
+let age = 1;
+// let grace = 2;
 
-console.log(age + grace)
-console.log(age * grace)
-console.log(age - grace)
-console.log(age / grace)
-console.log(age ** grace)
+// age += grace
+// age -= grace
 
-if((age+grace)>20){
+// console.log(age)
+// console.log(age + grace)
+// console.log(age * grace)
+// console.log(age - grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+if (age == 18) {
     console.log("You can visit our website");
 }
 
-else{
+else if (age == 0) {
+    console.log("are you kidding?");
+}
+
+else if (age == 1) {
+    console.log("are you again kidding?");
+}
+
+else {
     console.log("Sorry you cant visit our website");
 }
 
 // IN top statement we crated a variable age 40 then we say that "if" age is grater then 20 then show "you can visit our website" else show "you cant visit our website"
+
+// comments in javascript first is single line comment
+
+/*
+second is multi line comments
+*/
+
+a = 5;
+b = 10;
+
+let c = a > b ? (a - b) : (b - a);
+console.log(c)
+
+/*
+
+if(a>b) {
+let c = a - b;
+}
+
+else {
+    c = b - a;
+}
+
+*/
