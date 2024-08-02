2ae73865e5906b7add43116af60b3112e958c237
4ge101 <theforgettenone2@gmail.com>
2024-08-02 15:49:46 +0545

    practicing javascript problems

---
+++ JavaScript Practise/second JavaScript practise/index.js
@@ -0,0 +1,59 @@
+//problem 1
+
+let a = 10;
+let b = 20;
+
+c = a > b ? (a + b) : (b - a)
+console.log(c)
+
+
+//problem 2
+
+let age = 1;
+
+if (age == 18) {
+    console.log("You can visit our site")
+}
+
+else if (age == 0) {
+    console.log("your now child you cant visit this site")
+}
+
+else if (age == 1 || 5) {
+    console.log("your now child you cant visit this sit you need to 10year to visit this site")
+}
+
+else {
+    console.log("you cant visit our site")
+}
+
+//problem 3
+let d = 2;
+let e = 3;
+
+console.log(e / d)
+
+//problem 4
+let f = 2;
+let g = 3;
+
+if (f > g) {
+    console.log(f / g);
+}
+
+else {
+    console.log(f || g);
+}
+
+// problem 5
+
+let year = 20;
+let month = year > 18 ? console.log("you can drive") : console.log("You cant drive");
+
+// if (year>18){
+//     console.log("You can drive");
+// }
+
+// else {
+//     console.log("You cant drive");
+// }
