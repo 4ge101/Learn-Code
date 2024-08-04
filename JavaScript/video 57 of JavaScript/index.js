1fccf945f34acf9328b58aae6068fa78fe94d1ec
4ge101 <theforgettenone2@gmail.com>
2024-08-04 08:32:19 +0545

    learning all 5 types of loop JavaScript

--- JavaScript/video 57 of JavaScript/index.js
+++ JavaScript/video 57 of JavaScript/index.js
@@ -22,3 +22,26 @@ for (const key in obj) {
     const element = obj[key];
     console.log(key, element)
 }
+
+// for of loop
+
+for (const c of "Sami") {
+    console.log(c)
+}
+
+// while loop
+
+let i = 0;
+
+while (i < 10) {
+    console.log(i)
+    i++;
+}
+
+// do while loop
+let j = 10;
+
+do {
+    console.log(j)
+    j++;
+} while (j < 6);
