8ece07ad9659d235fa43f89e0866d7ef2a08134e
4ge101 <theforgettenone2@gmail.com>
2024-08-04 14:39:31 +0545

    making navigation menu responsive using javascript

---
+++ HTML WITH CSS PRACTISE/advanced-website/script.js
@@ -0,0 +1,9 @@
+function ShowSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
