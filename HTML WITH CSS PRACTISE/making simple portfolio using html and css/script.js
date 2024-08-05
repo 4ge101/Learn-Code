f290272309c6028608e0cff60c5ce9a43f49c8f3
4ge101 <theforgettenone2@gmail.com>
2024-08-05 21:19:14 +0545

    fixing background overflow in responsive

--- HTML WITH CSS PRACTISE/making simple portfolio using html and css/script.js
+++ HTML WITH CSS PRACTISE/making simple portfolio using html and css/script.js
@@ -1,16 +1,9 @@
-// var a = prompt("Enter your Name?")
-// console.log("Your Name is " + a)
+function ShowSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
 
-// console.log("Did wbsite working well")
-
-// var isTrue = confirm("Did you want to see this page because it is just a demo website")
-
-// if (isTrue) {
-//     alert("Now you can see this demo portfolio website")
-//     console.log("Now you can see this demo portfolio website")
-// }
-
-// else {
-//     alert("You dont want to see this site?")
-//     console.log("You dont want to see this site?")
-// }
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
