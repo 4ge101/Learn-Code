c2e2b7c3210f6e74e539b7b9641670860eca92ab
4ge101 <theforgettenone2@gmail.com>
2024-08-05 09:24:35 +0545

    removing unnessary overflow

---
+++ JavaScript/video 58 of JavaScript/ndex.js
@@ -0,0 +1,8 @@
+function hero(name) {
+    console.log("Hey " + name + " what is your name?");
+    console.log("My " + name + " name is Ali Sami!");
+    console.log("I " + name + " am a begginer in programming");
+    console.log("i " + name + " want to learn many skills");
+}
+
+hero("rohan");
