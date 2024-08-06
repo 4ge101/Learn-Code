e9caaa86e8ffc260be3ee6933eba9c449c634006
4ge101 <theforgettenone2@gmail.com>
2024-08-06 16:04:30 +0545

    leaning JAVASCRIPT

---
+++ JavaScript/video 60 of JavaScript/index.js
@@ -0,0 +1,15 @@
+console.log("This video is tutroial of string")
+
+let a = "Sami";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+// console.log(a[4]);
+
+console.log(a.length)
+
+let real_name = "Sami";
+let friend = "Rohan";
+console.log("His name is " + real_name + " And his friend name is " + friend);
+console.log(`His name is ${real_name} And his friend name is ${friend}`) 
