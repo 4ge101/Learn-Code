75ad27610ef195743007b69d117577bb8b2e3cae
4ge101 <theforgettenone2@gmail.com>
2024-08-06 18:56:45 +0545

    leaning strings in javascript

--- JavaScript/video 60 of JavaScript/index.js
+++ JavaScript/video 60 of JavaScript/index.js
@@ -13,3 +13,14 @@ let real_name = "Sami";
 let friend = "Rohan";
 console.log("His name is " + real_name + " And his friend name is " + friend);
 console.log(`His name is ${real_name} And his friend name is ${friend}`) 
+
+let b = "Akash";
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+console.log(b.length);
+console.log(b.slice(1,4));
+console.log(b.slice(1));
+console.log(b.replace("Ak", "77"))
+console.log(b.concat(a, "Rohan","shivam","krishna"))
+
+console.log(b)
