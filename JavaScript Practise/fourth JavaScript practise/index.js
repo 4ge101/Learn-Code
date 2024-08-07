30edb698f81522b5f9890e044ae1d676646c36b2
4ge101 <theforgettenone2@gmail.com>
2024-08-07 08:08:12 +0545

    practising javascript questions

--- JavaScript Practise/fourth JavaScript practise/index.js
+++ JavaScript Practise/fourth JavaScript practise/index.js
@@ -1,1 +1,39 @@
+// PROBLEM 1
+
 console.log("har\"".length)
+
+// PROBLEM 2
+
+let str1 = 'Cats are the best'
+console.log(str1.endsWith('best'))
+
+let str2 = 'Saturday night plans'
+console.log(str2.startsWith('Sat'))
+
+//PROBLEM 3
+
+let num1 = 'Sami'
+console.log(num1.toLowerCase())
+
+// PROBLEM 4
+
+let str3 = "PLEASE GIVE RS 1000"
+
+if (str3 || 1000) {
+    console.log(str3)
+}
+else {
+    console.log("YOU COMPLETE YOU PAYMENT")
+}
+
+//PROBLEM 5
+// let str3 = "PLEASE take RS 1000"
+
+// if (str3 < 1000) {
+//     console.log("PLEASE GIVE RS 1000")
+// }
+// else {
+//     console.log("YOU COMPLETE YOU PAYMENT")
+// }
+
+// IN PROBLEM 5 WE CANT CHANGE THE STRING
