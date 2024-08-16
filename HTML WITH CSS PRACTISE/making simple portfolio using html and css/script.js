5668c375a43a00635c317542b84e3f7775025dce
4ge101 <theforgettenone2@gmail.com>
2024-08-16 18:43:48 +0545

    working on portfolio website

--- HTML WITH CSS PRACTISE/making simple portfolio using html and css/script.js
+++
@@ -1,9 +0,0 @@
-function ShowSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
-}
-
-function hideSidebar() {
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
-}
--- JavaScript/video 63 of JavaScript/index.js
+++ JavaScript/video 63 of JavaScript/index.js
@@ -2,10 +2,20 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
 arr[0] = 44
 
-console.log(arr)
-console.log(arr.length)
+a = [1,2,3,4]
+// console.log(arr)
+// console.log(arr.length, typeof arr)
 
-console.log(arr[0])
-console.log(arr[2])
-console.log(arr[4])
-console.log(arr[6])
+// console.log(arr[0])
+// console.log(arr[2])
+// console.log(arr[4])
+// console.log(arr[6])
+
+// to string
+console.log(arr.toString())
+// to join 
+console.log(arr.join(" and "))
+// to pop 
+console.log(arr.pop())
+// to push 
+console.log(a.push("Sami"))
