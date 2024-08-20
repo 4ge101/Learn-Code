b346c0cf2e778f278906e46439382d23e019b065
4ge101 <theforgettenone2@gmail.com>
2024-08-20 21:11:22 +0545

    making random password geneator

---
+++ WEB_PROJECTS/index.js
@@ -0,0 +1,17 @@
+/* Function to generate combination of password */
+function generatePass() {
+    let pass = '';
+    let str = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ' +
+        'abcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+    for (let i = 1; i <= 8; i++) {
+        let char = Math.floor(Math.random()
+            * str.length + 1);
+
+        pass += str.charAt(char)
+    }
+
+    return pass;
+}
+
+console.log(generatePass());
