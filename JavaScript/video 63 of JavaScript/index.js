937952a70155e960e44b0ffa4e5b857149e92362
4ge101 <theforgettenone2@gmail.com>
2024-08-21 08:15:20 +0545

    learning array

--- JavaScript/video 63 of JavaScript/index.js
+++ JavaScript/video 63 of JavaScript/index.js
@@ -3,6 +3,11 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8]
 arr[0] = 44
 
 a = [1,2,3,4]
+
+let a1 = [1,2,3]
+let a2 = [4,5,6]
+let a3 = [7,8,9]
+
 // console.log(arr)
 // console.log(arr.length, typeof arr)
 
@@ -19,3 +24,11 @@ console.log(arr.join(" and "))
 console.log(arr.pop())
 // to push 
 console.log(a.push("Sami"))
+// to shift
+console.log(a.shift())
+// to unshift
+console.log(a.unshift("JACK"))
+// to delete
+console.log(delete a[4])
+// to concat
+console.log(a1.concat(a2,a3))
