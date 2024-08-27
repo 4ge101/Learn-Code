1c5b83bf3fab596587b79df59a4004ff9a21a8ce
4ge101 <theforgettenone2@gmail.com>
2024-08-27 09:30:14 +0545

    learning for and for in loop in arrays

--- JavaScript/video 63 of JavaScript/index.js
+++ JavaScript/video 63 of JavaScript/index.js
@@ -2,11 +2,17 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
 arr[0] = 44
 
-a = [1,2,3,4]
+a = [1, 2, 3, 4]
 
-let a1 = [1,2,3]
-let a2 = [4,5,6]
-let a3 = [7,8,9]
+let a1 = [1, 2, 3]
+let a2 = [4, 5, 6]
+let a3 = [7, 8, 9]
+
+let b = [2, 1, 3]
+
+let numbers = [1, 2, 3, 4, 5]
+
+let c = [1, 2, 3, 4]
 
 // console.log(arr)
 // console.log(arr.length, typeof arr)
@@ -31,4 +37,34 @@ console.log(a.unshift("JACK"))
 // to delete
 console.log(delete a[4])
 // to concat
-console.log(a1.concat(a2,a3))
+console.log(a1.concat(a2, a3))
+// to sort
+console.log(b.sort())
+// to splice
+console.log(numbers.splice(1, 3, 44, 55))
+// to slice
+console.log(c.slice(1, 3))
+// to reverse
+console.log(c.reverse())
+
+// loops
+let d = [1, 32, 5, 88]
+
+d.forEach((value, index, arr) => {
+    console.log(value, index, arr)
+})
+
+// for in loop
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+for (const key in obj) {
+    if (obj.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        console.log(key, element)
+    }
+}
