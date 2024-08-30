c0f37de991da74c5f1cb984b9b61af9bea1376c7
4ge101 <theforgettenone2@gmail.com>
2024-08-30 21:17:20 +0545

    started to make portfolio responsive

---
+++ HTML WITH CSS PRACTISE/making simple portfolio using html and css/script.js
@@ -0,0 +1,9 @@
+function showSidebar() {
+    const sidebar_menu = document.querySelector('.sidebar-menu');
+    sidebar_menu.style.display = 'flex';
+}
+
+function hideSidebar() {
+    const sidebar_menu = document.querySelector('.sidebar-menu');
+    sidebar_menu.style.display = 'none';
+}
