542c1581173bf4de13268de6ccd476e6f36bdd59
4ge101 <theforgettenone2@gmail.com>
2024-09-20 08:21:01 +0545

    learning second video of typescript

---
+++ TypeScript/video_2_of_TypeScript/app.js
@@ -0,0 +1,13 @@
+var num;
+var string = "hello world";
+var isTrue = false;
+function add(n1, n2) {
+    return n1 + n2;
+}
+var result = add(10, 20);
+console.log("result");
+function dummy(a) {
+    return a;
+}
+var answer = dummy("hello dummy");
+console.log("answer");
---
+++ TypeScript/video_2_of_TypeScript/app.ts
@@ -0,0 +1,18 @@
+let num: number;
+let string: string = "hello world";
+let isTrue: boolean = false;
+
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+let result = add(10, 20);
+console.log("result");
+
+
+function dummy(a: string) {
+    return a;
+}
+
+let answer = dummy("hello dummy")
+console.log("answer");
