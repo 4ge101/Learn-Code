e558d7eee512091dc557b567efce270f7188ba76
4ge101 <theforgettenone2@gmail.com>
2024-10-10 11:56:07 +0545

    learn typescript

---
+++ TypeScript/full_typescript_course/app.ts
@@ -0,0 +1,22 @@
+/* 
+BASIC TYPES 
+1) Primitive types (number, string, boolean)
+2) Arrays
+3) Tuples
+4) Enums
+5) Any, Unknown, Void, Null, Undefined, Never
+
+*/
+
+/* () {} [] 
+    if we used any of thes brackets then it's value is reference
+    if there is not any bracket is used then it is primitive
+*/
+
+// NUMBER STRING AND BOOLEAN
+let a = 12; //numbers
+let b = 'Sami'; //string
+let c = true; //boolean
+
+// Arrays
+let arr = [1, 2, 3, 4, 5];
