c191e7f51b94e80747dfcebc8f5119e2abb5eff4
4ge101 <theforgettenone2@gmail.com>
2024-10-14 19:31:03 +0545

    learning typescript

--- TypeScript/full_typescript_course/app.js
+++ TypeScript/full_typescript_course/app.js
@@ -18,3 +18,24 @@ let b = 'Sami'; //string
 let c = true; //boolean
 // Arrays
 let arr = [1, 2, 3, 4, 5];
+//Tuples
+// it is used to define the values of array or anything in same positipn also if we try to change the position then it show the error for example
+let array = ["Sami", 43];
+let array2 = [100, "Sami"];
+//Enums also known as "Enumerations" it is used to hold the set of values for example 
+var UserRoles;
+(function (UserRoles) {
+    UserRoles["ADMIN"] = "admin";
+    UserRoles["GUEST"] = "guest";
+    UserRoles["SUPER_ADMIN"] = "super_admin";
+})(UserRoles || (UserRoles = {}));
+// Any, Unknown, Void, Null, Undefined, Never
+// any if we dont define the type of variable it is "any" for example
+let i;
+// how to define the type of any variable 
+let v;
+v = 44;
+// Unknown
+let u;
+u = 12;
+u = "sami";
--- TypeScript/full_typescript_course/app.ts
+++ TypeScript/full_typescript_course/app.ts
@@ -20,3 +20,29 @@ let c = true; //boolean
 
 // Arrays
 let arr = [1, 2, 3, 4, 5];
+
+//Tuples
+// it is used to define the values of array or anything in same positipn also if we try to change the position then it show the error for example
+let array: [string, number] = ["Sami", 43]
+let array2: [number, string] = [100, "Sami"]
+
+//Enums also known as "Enumerations" it is used to hold the set of values for example 
+
+enum UserRoles {
+    ADMIN = "admin",
+    GUEST = "guest",
+    SUPER_ADMIN = "super_admin"
+}
+
+// Any, Unknown, Void, Null, Undefined, Never
+// any if we dont define the type of variable it is "any" for example
+let i;
+
+// how to define the type of any variable 
+let v: number;
+v = 44;
+
+// Unknown
+let u: unknown;
+u = 12;
+u = "sami";
